6c4f80c8b23d743b2d702d2bd04053fdef4c09a7
nehemiatelusma <nehemia.telusma@akqa.com>
2025-10-15 14:50:46 +0200

    feat: handle character animation & physics

---
+++ src/components/Experience/Character/MovementController.js
@@ -0,0 +1,133 @@
+import RAPIER from "@dimforge/rapier3d-compat";
+
+export default class MovementController {
+  constructor(character) {
+    this.character = character;
+    this.characterController = character.characterController;
+    this.rigidbody = character.rigidbody;
+    this.model = character.model;
+    this.physicsWorld = character.physicsWorld;
+
+    this.walkSpeed = 5;
+    this.runSpeed = 8;
+    this.jumpForce = 15;
+    
+    this.groundFriction = 0.8;
+    this.airFriction = 0.95;
+    
+    this.modelYOffset = -1.0;
+  }
+
+
+  checkGrounded() {
+    if (!this.rigidbody) return;
+
+    const vel = this.rigidbody.linvel();
+    const pos = this.rigidbody.translation();
+
+
+    const isGrounded = Math.abs(vel.y) < 0.1 && pos.y < 1.0;
+    
+    if (this.characterController) {
+      this.characterController.setGrounded(isGrounded);
+    }
+  }
+
+
+  handleMovement() {
+    if (!this.rigidbody || !this.characterController) return;
+
+    const horizontalAxis = this.characterController.getHorizontalInput();
+    const currentSpeed = this.characterController.getCurrentSpeed();
+
+    if (Math.abs(horizontalAxis) > 0.01) {
+      const currentVel = this.rigidbody.linvel();
+
+
+      this.rigidbody.setLinvel(
+        new RAPIER.Vector3(
+          horizontalAxis * currentSpeed,
+          currentVel.y,
+          currentVel.z
+        ),
+        true
+      );
+
+
+      this.updateModelRotation(horizontalAxis);
+    } else {
+
+      this.applyFriction();
+    }
+  }
+
+
+  applyFriction() {
+    if (!this.rigidbody) return;
+
+    const currentVel = this.rigidbody.linvel();
+    const isGrounded = this.characterController?.getIsGrounded() || false;
+    
+
+    const friction = isGrounded ? this.groundFriction : this.airFriction;
+
+    this.rigidbody.setLinvel(
+      new RAPIER.Vector3(currentVel.x * friction, currentVel.y, currentVel.z),
+      true
+    );
+  }
+
+
+  updateModelRotation(direction) {
+    if (!this.model) return;
+
+    if (direction > 0) {
+      this.model.rotation.y = Math.PI / 2; 
+    } else if (direction < 0) {
+      this.model.rotation.y = -Math.PI / 2; 
+    }
+  }
+
+  jump(force = this.jumpForce) {
+    if (!this.rigidbody || !this.characterController) return;
+    
+    if (this.characterController.getIsGrounded()) {
+      const currentVel = this.rigidbody.linvel();
+      
+      this.rigidbody.setLinvel(
+        new RAPIER.Vector3(currentVel.x, force, currentVel.z),
+        true
+      );
+      
+      this.characterController.setGrounded(false);
+    }
+  }
+
+
+  syncModelPosition() {
+    if (!this.rigidbody || !this.model) return;
+
+    const translation = this.rigidbody.translation();
+    this.model.position.set(
+      translation.x,
+      translation.y + this.modelYOffset,
+      translation.z
+    );
+  }
+  
+  update() {
+    this.checkGrounded();
+
+    this.handleMovement();
+
+    this.syncModelPosition();
+  }
+
+  destroy() {
+    this.rigidbody = null;
+    this.model = null;
+    this.character = null;
+    this.characterController = null;
+  }
+}
+
