2a56a65522d582d66eddb4ee4f57b54749a049b7
nehemiatelusma <nehemia.telusma@akqa.com>
2025-10-15 14:47:46 +0200

    file: adding character & fast run animation

--- src/components/Experience/sources.js
+++ src/components/Experience/sources.js
@@ -1,26 +1,17 @@
 export default [
-  // Put the files in the Public folder
-  // {
-  //   name: "environmentMapTexture",
-  //   type: "cubeTexture",
-  //   path: [
-  //     "/textures/environmentMap/px.jpg",
-  //     "/textures/environmentMap/nx.jpg",
-  //   ],
-  // },
-  // {
-  //   name: "grassColorTexture",
-  //   type: "texture",
-  //   path: "/textures/dirt/color.jpg",
-  // },
-  // {
-  //   name: "grassNormalTexture",
-  //   type: "texture",
-  //   path: "/textures/dirt/normal.jpg",
-  // },
   {
     name: "mapModel",
     type: "gltf",
     path: "/models/Map/test.glb",
+  },
+  {
+    name: "characterModel",
+    type: "fbx",
+    path: "/models/Character/character.fbx",
+  },
+  {
+    name: "fastRunAnim",
+    type: "fbx",
+    path: "/models/Character/fast_run_anim.fbx",
   },
 ];
