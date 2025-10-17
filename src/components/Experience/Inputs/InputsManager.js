61e47207c5a3f2f1f581fd60a842893202603f06
nehemiatelusma <nehemia.telusma@akqa.com>
2025-10-17 14:38:20 +0200

    fix: inputs

--- src/components/Experience/Inputs/InputsManager.js
+++ src/components/Experience/Inputs/InputsManager.js
@@ -13,12 +13,10 @@ export default class InputManager extends EventEmitter {
 
     this.joystickValues = {
       joystick1: { x: 0, y: 0 },
-      joystick2: { x: 0, y: 0 },
     };
 
     this.Axis = null;
     this.gamepadEmulator1 = null;
-    this.gamepadEmulator2 = null;
 
     // Only initialize Axis API on the client side
     if (typeof window !== 'undefined') {
@@ -42,17 +40,15 @@ export default class InputManager extends EventEmitter {
   setupKeyboardEmulation() {
     if (!this.Axis) return;
     
-    this.Axis.registerKeys("q", "a", 1);      
-    this.Axis.registerKeys("d", "x", 1);      
-    this.Axis.registerKeys("z", "i", 1);      
-    this.Axis.registerKeys("s", "s", 1);      
-    this.Axis.registerKeys(" ", "w", 1);      
-      
-    this.Axis.registerKeys("ArrowLeft", "a", 2);
-    this.Axis.registerKeys("ArrowRight", "x", 2);
-    this.Axis.registerKeys("ArrowUp", "i", 2);
-    this.Axis.registerKeys("ArrowDown", "s", 2);
-    this.Axis.registerKeys("Enter", "w", 2);
+    this.Axis.registerKeys(["ArrowUp", "z"], "a", 1);
+    this.Axis.registerKeys(" ", "x", 1);
+
+    this.setupKeyboardMovement();
+  }
+
+  setupKeyboardMovement() {
+    window.addEventListener("keydown", this.handleDirectKeyDown.bind(this));
+    window.addEventListener("keyup", this.handleDirectKeyUp.bind(this));
   }
 
   setupGamepadEmulation() {
@@ -64,21 +60,15 @@ export default class InputManager extends EventEmitter {
         this.gamepadEmulator1 = this.Axis.createGamepadEmulator(0);
         this.Axis.joystick1.setGamepadEmulatorJoystick(this.gamepadEmulator1, 0);
 
-        this.Axis.registerGamepadEmulatorKeys(this.gamepadEmulator1, 0, "w", 1);
-        this.Axis.registerGamepadEmulatorKeys(this.gamepadEmulator1, 1, "u", 1); 
-      }
-
-      if (gamepads[1]) {
-        this.gamepadEmulator2 = this.Axis.createGamepadEmulator(1);
-        this.Axis.joystick2.setGamepadEmulatorJoystick(this.gamepadEmulator2, 0);
-        this.Axis.registerGamepadEmulatorKeys(this.gamepadEmulator2, 0, "w", 2);
+        this.Axis.registerGamepadEmulatorKeys(this.gamepadEmulator1, 0, "a", 1);
+        this.Axis.registerGamepadEmulatorKeys(this.gamepadEmulator1, 2, "x", 1);
       }
     };
 
     setTimeout(checkGamepad, 100);
 
     window.addEventListener("gamepadconnected", () => {
-      console.log("Gamepad connecté");
+      console.log("Gamepad connected");
       checkGamepad();
     });
   }
@@ -93,52 +83,49 @@ export default class InputManager extends EventEmitter {
       "joystick:move",
       this.handleJoystick1Move.bind(this)
     );
-    this.Axis.joystick2.addEventListener(
-      "joystick:move",
-      this.handleJoystick2Move.bind(this)
-    );
+  }
+
+  handleDirectKeyDown(e) {
+    if (e.key === "ArrowLeft") {
+      this.keys.left = true;
+    }
+    if (e.key === "ArrowRight") {
+      this.keys.right = true;
+    }
+  }
+
+  handleDirectKeyUp(e) {
+    if (e.key === "ArrowLeft") {
+      this.keys.left = false;
+    }
+    if (e.key === "ArrowRight") {
+      this.keys.right = false;
+    }
   }
 
   handleKeyDown(e) {
     if (e.key === "a") {
-      this.keys.left = true;
-      this.trigger("move:left:start");
+      this.keys.run = true;
+      console.log("run:start");
+      
+      this.trigger("run:start");
     }
 
     if (e.key === "x") {
-      this.keys.right = true;
-      this.trigger("move:right:start");
-    }
-
-    if (e.key === "w") {
       this.keys.jump = true;
+      console.log("jump");
       this.trigger("jump");
-    }
-
-    if (e.key === "i") {
-      this.keys.run = true;
-      this.trigger("run:start");
     }
   }
 
   handleKeyUp(e) {
     if (e.key === "a") {
-      this.keys.left = false;
-      this.trigger("move:left:end");
+      this.keys.run = false;
+      this.trigger("run:end");
     }
 
     if (e.key === "x") {
-      this.keys.right = false;
-      this.trigger("move:right:end");
-    }
-
-    if (e.key === "w") {
       this.keys.jump = false;
-    }
-
-    if (e.key === "i") {
-      this.keys.run = false;
-      this.trigger("run:end");
     }
   }
 
@@ -148,25 +135,17 @@ export default class InputManager extends EventEmitter {
     this.trigger("joystick:move", { id: 1, ...e });
   }
 
-  handleJoystick2Move(e) {
-    this.joystickValues.joystick2.x = e.position.x;
-    this.joystickValues.joystick2.y = e.position.y;
-    this.trigger("joystick:move", { id: 2, ...e });
-  }
-
   getHorizontalAxis() {
     let axis = 0;
 
     const joystick1X = this.joystickValues.joystick1.x;
-    const joystick2X = this.joystickValues.joystick2.x;
-
     if (Math.abs(joystick1X) > 0.1) {
       axis = joystick1X;
-    } else if (Math.abs(joystick2X) > 0.1) {
-      axis = joystick2X;
-    } else {
-      if (this.keys.left) axis -= 1;
-      if (this.keys.right) axis += 1;
+    }
+    else if (this.keys.left) {
+      axis = -1;
+    } else if (this.keys.right) {
+      axis = 1;
     }
 
     return axis;
@@ -184,12 +163,14 @@ export default class InputManager extends EventEmitter {
     if (this.gamepadEmulator1) {
       this.gamepadEmulator1.update();
     }
-    if (this.gamepadEmulator2) {
-      this.gamepadEmulator2.update();
-    }
   }
 
   destroy() {
+    if (typeof window !== 'undefined') {
+      window.removeEventListener("keydown", this.handleDirectKeyDown.bind(this));
+      window.removeEventListener("keyup", this.handleDirectKeyUp.bind(this));
+    }
+
     if (!this.Axis) return;
     
     this.Axis.removeEventListener("keydown", this.handleKeyDown.bind(this));
@@ -197,10 +178,6 @@ export default class InputManager extends EventEmitter {
     this.Axis.joystick1.removeEventListener(
       "joystick:move",
       this.handleJoystick1Move.bind(this)
-    );
-    this.Axis.joystick2.removeEventListener(
-      "joystick:move",
-      this.handleJoystick2Move.bind(this)
     );
   }
 }
