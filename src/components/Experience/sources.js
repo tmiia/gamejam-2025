5419ff80a09ddf4ff27c2d8bbf6650ca0c795149
Martin Cantonny <cantonny.martin16@gmail.com>
2025-10-23 15:03:16 +0200

    feat : good map

--- src/components/Experience/sources.js
+++ src/components/Experience/sources.js
@@ -2,12 +2,12 @@ export default [
   {
     name: "mapModel",
     type: "gltf",
-    path: "/models/Map/Map_35.glb",
+    path: "/models/Map/Map_37.glb",
   },
   {
     name: "mapModelPhysics",
     type: "gltf",
-    path: "/models/Map/Map_35.glb",
+    path: "/models/Map/Map_37.glb",
   },
   {
     name: "background",
